fc4b47ea6c039c4921fa636a43d9c5ce5548fc9d
Руслан Ахриев <ruslanahriev@MacBook-Pro-Ruslan.local>
2017-09-28 09:43:56 +0700

    Initial Commit

---
+++ Survey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Survey
+//
+//  Created by Руслан Ахриев on 28.09.2017.
+//  Copyright © 2017 Руслан Ахриев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
